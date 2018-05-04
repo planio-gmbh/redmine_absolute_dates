14e13cfcd50bcc2a86c51c03df0488e32166d192
Jens Kraemer <jk@jkraemer.net>
2018-05-04 10:05:34 +0800

    version bump, metadata, require a reasonably current version of Redmine

--- init.rb
+++ init.rb
@@ -6,9 +6,10 @@ end
 
 Redmine::Plugin.register :redmine_absolute_dates do
   name 'Redmine Absolute Dates plugin'
-  author 'suer'
+  author 'suer, Jens Kraemer (Planio GmbH)'
   description 'Display absolute create or update dates '
-  version '0.0.3'
-  url 'https://github.com/suer/redmine_absolute_dates'
+  version '0.1.0'
+  url 'https://github.com/planio-gmbh/redmine_absolute_dates'
   author_url 'http://d.hatena.ne.jp/suer'
+  requires_redmine version_or_higher: '3.3.0'
 end
